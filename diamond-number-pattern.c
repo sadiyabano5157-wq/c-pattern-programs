ba03dbd9ebaf1a5f03e3cec0d85e23719c28fa30
sadiyabano5157-wq <sadiyabano5157@gmail.com>
2026-05-26 21:15:46 +0530

    Add diamond number pattern program in C
    This program prompts the user for a row number and prints a diamond pattern of numbers based on the input.

---
+++ diamond-number-pattern.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter a row:");
+	scanf("%d",&n);
+	
+
+
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=n-i;j++)
+		{
+			printf(" ");
+		}
+    	for(int j=1;j<=2*i-1;j++)
+		{
+		
+		 printf("%d",i);	
+		
+		}
+		printf("\n");
+	}
+	
+    	for(int i=n-1;i>=1;i--)	
+	{
+		  for(int j=1;j<=n-i;j++)
+		  {
+		  	printf(" ");
+		  }
+		for(int j=1;j<=2*i-1;j++)
+		{
+			printf("%d",i);
+		}
+		printf("\n");
+	}
+	return 0;
+}
